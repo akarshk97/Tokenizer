e945b46514830e1847d6e985398b76365eabfafe
Akarsh Kashamshetty <akarsh9076@gmail.com>
2021-02-18 20:17:41 -0500

    tokens in dir and freq in file

--- project/main.py
+++ project/main.py
@@ -6,17 +6,25 @@ import glob
 
 inputDir = sys.argv[1]
 outputDir = sys.argv[2]
+print(outputDir)
 entries = os.listdir(inputDir)
-files = glob.glob('/outputDir/*.txt')
+files = glob.glob("/"+outputDir +'*.txt')
 counter = 0
+
 for f in files:
     try:
         f.unlink()
     except OSError as e:
         print("Error: %s : %s" % (f, e.strerror))
 
-def freq(str, prefix): 
-  
+def writeTokens(str, prefix):
+    print(str)
+    str_list = str.split()
+    with open(prefix + 'tokens.txt','w') as log:
+        for i in str_list:
+            log.write('{}\n'.format(i))
+    
+def freq(str): 
     # break the string into list of words 
     str_list = str.split() 
     tokenDict = {}
@@ -30,15 +38,15 @@ def freq(str, prefix):
     # sorting the token dict in descending order
     sortedTokens = {k: v for k, v in sorted(tokenDict.items(), key=lambda item: item[1], reverse = True) }
     # writing tokens with frequency into log file
-    with open(prefix + 'log.txt','w') as log:
+    with open('sortedTokens.txt','w') as log:
         for key in sortedTokens:
             log.write('{} {}\n'.format(key, sortedTokens[key]))
-
+wholeString = ""
 for i in entries:
     print("current file", counter)
     counter = counter + 1
     prefix = i.split(".")
-    path = "/Users/akarshkashamshetty/OneDrive - UMBC/grad_sem4/CMSC676/project/files/" + i
+    path = "/Users/akarshkashamshetty/OneDrive - UMBC/grad_sem4/CMSC676/project/" + inputDir + i
 
     with open(path, 'rb') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
@@ -49,8 +57,11 @@ for i in entries:
 
     #cleaning the string by removing characters other than alphanumeric and spaces 
     cleanString = re.sub('[^A-Za-z0-9 ]+', '', text)
+    wholeString = wholeString + cleanString
+    writeTokens(cleanString, outputDir +"/"+ prefix[0])
 
-    freq(cleanString, outputDir +"/"+ prefix[0])
+freq(wholeString)
+
 
     
     
